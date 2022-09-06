d0e56680c8bb26b19b2badadacd4053293424055
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-09-06 23:44:36 +0900

    파일 업로드 기능

---
+++ ApiHelloWorld/Controllers/WebApiFileUploadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace ApiHelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebApiFileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+
+        public WebApiFileUploadController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(IEnumerable<IFormFile> files)
+        {
+            var uploadFolder = Path.Combine(this.environment.WebRootPath, "files");
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var fileName = Path.GetFileName(ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim().ToString());
+
+                    using var fileStream = new FileStream(Path.Combine(uploadFolder, fileName), FileMode.Create);
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+
+            return Ok(new { message = "OK" });
+        }
+    }
+}
---
+++ ApiHelloWorld/Controllers/WebApiFileUploadDemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHelloWorld.Controllers
+{
+    public class WebApiFileUploadDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
