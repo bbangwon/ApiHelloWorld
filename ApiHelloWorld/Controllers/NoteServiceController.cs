d8f5a5a09364d1377e5b32b4643fcef59b59951e
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-09-09 22:13:13 +0900

    api versioning study

--- ApiHelloWorld/Controllers/NoteServiceController.cs
+++ ApiHelloWorld/Controllers/NoteServiceController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiHelloWorld.Controllers
 {
+    [ApiVersion("1.0")] //API 버전
     [Route("api/[controller]")]
     [ApiController]
     public class NoteServiceController : ControllerBase
@@ -31,7 +32,6 @@ namespace ApiHelloWorld.Controllers
         }
 
         [HttpGet("{id}")]
-
         public IActionResult Get(int id)
         {
             var note = repository.GetById(id);
@@ -83,6 +83,101 @@ namespace ApiHelloWorld.Controllers
         {
             var removed = repository.Delete(id);
             if(!removed)
+            {
+                return NotFound($"{id}번 데이터가 없습니다.");
+            }
+            return NoContent();
+        }
+    }
+}
+
+namespace ApiHelloWorld.Controllers.V2
+{
+    [ApiVersion("2.0")] //API 버전
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteServiceController : ControllerBase
+    {
+        private readonly INoteRepository repository;
+        private readonly ILogger logger;
+
+        public NoteServiceController(INoteRepository repository, ILoggerFactory logger)
+        {
+            this.repository = repository;
+            this.logger = logger.CreateLogger(nameof(NoteServiceController));
+        }
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public IActionResult Get()
+        {
+            logger.LogInformation("로그 출력해봄");
+
+            var notes = repository.GetAll();
+            if (notes == null)
+            {
+                return NotFound($"아무런 데이터가 없습니다.");
+            }
+            return Ok(notes.OrderBy(n => n.Id));
+        }
+
+        [HttpGet("{id}")]
+        [MapToApiVersion("2.0")]
+        public IActionResult Get(int id)
+        {
+            var note = repository.GetById(id);
+            if (note == null)
+            {
+                return NotFound($"{id}번 데이터가 없습니다.");
+            }
+
+            return Ok(note);
+        }
+
+        [HttpGet("{page}/{pageSize}")]
+        [MapToApiVersion("2.0")]
+        public IActionResult Get(int page = 0, int pageSize = 10)
+        {
+            var notes = repository.GetAllWithPaging(page, pageSize);
+            if (notes == null || !notes.Any())
+            {
+                return NotFound($"아무런 데이터가 없습니다.");
+            }
+
+            //헤더에 총 레코드 수를 담아서 출력
+            Response.Headers.Add("X-TotalRecordCount", repository.GetRecordCount().ToString());
+
+            return Ok(notes);
+        }
+
+        [HttpPost]
+        [MapToApiVersion("2.0")]
+        public IActionResult Post([FromBody] Note note)
+        {
+            var newNote = repository.Add(note);
+            return CreatedAtAction(nameof(Get), new { id = newNote.Id }, newNote);
+        }
+
+        [HttpPut("{id}")]
+        [MapToApiVersion("2.0")]
+        public IActionResult Put(int id, [FromBody] Note note)
+        {
+            note.Id = id;
+            var updateNote = repository.Update(note);
+            if (updateNote == null)
+            {
+                return NotFound($"{id}번 데이터가 없습니다.");
+            }
+
+            return NoContent(); //이미 던져준 정보에 모든 값 가지고 있기에..
+        }
+
+        [HttpDelete("{id}")]
+        [MapToApiVersion("2.0")]
+        public IActionResult Delete(int id)
+        {
+            var removed = repository.Delete(id);
+            if (!removed)
             {
                 return NotFound($"{id}번 데이터가 없습니다.");
             }
--- ApiHelloWorld/Program.cs
+++ ApiHelloWorld/Program.cs
@@ -1,6 +1,10 @@
 using ApiHelloWorld.Component;
 using ApiHelloWorld.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +18,26 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//버저닝 사용
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);   //기본API버전
+    options.ReportApiVersions = true;   //클라이언트 응답헤더에 버전표시
+    options.AssumeDefaultVersionWhenUnspecified = true; //따로 버전을 지정하지 않을 경우 기본 버전을 사용
+    options.ApiVersionReader = new HeaderApiVersionReader("X-Api-Version"); //헤더에 버전 정보 보내기
+});
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiHelloWorld - V1", Version = "v1" });
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "ApiHelloWorld - V2", Version = "v2" });
+});
 
 builder.Services.AddScoped<IPointRepository, PointRepositoryInMemory>();
 builder.Services.AddScoped<IPointLogRepository, PointLogRepository>();
@@ -25,8 +48,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(o => {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            o.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", $"{description.GroupName.ToUpper()}");
+        }
+    });
 }
 
 app.UseStaticFiles();
